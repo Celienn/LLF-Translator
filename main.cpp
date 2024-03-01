52bca720eb81b68addc889234196dc65a2e0bc64
Celienn <fogueteiro.celien.pro@gmail.com>
2024-03-01 09:38:05 +0100

    kado clement

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 
 #include <QApplication>
 
+vector<string> split(string str, string delimiter){
+    size_t pos = 0;
+    string token;
+    vector<string> tokens;
+
+    while ((pos = str.find(delimiter)) != string::npos) {
+        token = str.substr(0, pos);
+        tokens.push_back(token);
+        str.erase(0, pos + delimiter.length());
+    }
+
+    tokens.push_back(str);
+    return tokens;
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -10,5 +25,6 @@ int main(int argc, char *argv[])
     w.show();
     LLFTranslator llf;
     llf.connect();
+    llf.readVar();
     return a.exec();
 }
