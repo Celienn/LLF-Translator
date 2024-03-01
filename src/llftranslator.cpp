592a38f885a0def76660b65b4089ddc2e1d1fe88
Nayl <96319162+Nayyl@users.noreply.github.com>
2024-03-01 15:47:37 +0100

    Ajout fonction pour load la config pas encore testé sur windows
    premier ajout

--- src/llftranslator.cpp
+++ src/llftranslator.cpp
@@ -83,3 +83,53 @@ void LLFTranslator::connect() {
         qDebug() << "Failed to connect to MFS!";
     }
 }
+
+vector<string> LLFTranslator::loadConfig()
+{
+    vector<string> table;
+    QFile file(":/config.csv");
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "Impossible d'ouvrir le fichier : " << file.errorString();
+        return table;
+    }
+
+    QTextStream in(&file);
+    std::string fileContent;
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        fileContent += line.toStdString() + ',';
+    }
+
+    table = split(fileContent, ",");
+    config = table;
+    file.close();
+    return table;
+}
+
+vector<string> split(string str, string delimiter){
+    size_t pos = 0;
+    string token;
+    vector<string> tokens;
+
+    while ((pos = str.find(delimiter)) != string::npos) {
+        token = str.substr(0, pos);
+        tokens.push_back(token);
+        str.erase(0, pos + delimiter.length());
+    }
+
+    tokens.push_back(str);
+    return tokens;
+}
+
+string LLFTranslator::translateXPlaneToMFS(string ref)
+{
+    for(int i = 0 ;i < (int)config.size()-1 ;i++){
+        if (config[i] == ref) continue;
+        {
+            return config[i + 1];
+        }
+    }
+    return "Not Found";
+}
+
