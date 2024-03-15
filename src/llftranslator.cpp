c7db740687338c472cdde6f1105cef360c29457b
Nayl <96319162+Nayyl@users.noreply.github.com>
2024-03-15 17:18:27 +0100

    Update llftranslator.cpp
    sup de la méthode split qui ne servait qu'une seule fois dans l'ancienne methode pour load

--- src/llftranslator.cpp
+++ src/llftranslator.cpp
@@ -57,21 +57,6 @@ void LLFTranslator::connect() {
     }
 }
 
-vector<string> split(string str, string delimiter){
-    size_t pos = 0;
-    string token;
-    vector<string> tokens;
-
-    while ((pos = str.find(delimiter)) != string::npos) {
-        token = str.substr(0, pos);
-        tokens.push_back(token);
-        str.erase(0, pos + delimiter.length());
-    }
-
-    tokens.push_back(str);
-    return tokens;
-}
-
 QList<QString> loadConfig()
 {
     QList<QString> list;
